2737ce0b79c2cc1d7645319c2fcaba4aa950d6b2
e-wj-m <eric.milroy@hotmail.ca>
2025-07-17 21:21:25 -0400

    Part Two - Player Controller and Background Sprite Repetition
    Managed to connect the Player Controller to Unity's Input manager, allowing for horizontal Player movement using the arrow keys, and Jumps using the space bar.
    
    Player Movement will require further tweaking to fix unlimited jumps, as well as jump height to ensure it's consistent across my static obstacles in scene.
    
    Managed to include a repeating texture as my background, utilizing a Material and a Quad box in my hierarchy, will also require further tweaking as I continue to paint my level.
    
    Did not manage to finish painting level in it's entirety in this step, but will continue to paint level in coming steps until completion.
    
    Next steps include adding custom level colliders, organizing layers (Level, Background, Far Background etc), and possibly death boxes to ensure is player does fall during platforming, it isn't 'out of map' per se, just a 'death' state. Though that may be included in Step 3.
    
    -E

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D rb;  
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        float hValue = Input.GetAxis("Horizontal");
+
+        rb.linearVelocityX = hValue * 5f;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+        }
     }
 }

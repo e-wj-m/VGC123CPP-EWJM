bb62722c6c27cbb76748ab61517ae0eefc4f9c32
e-wj-m <eric.milroy@hotmail.ca>
2025-07-31 16:59:24 -0400

    Part Five - Core Animations Complete, Dive Attack & Attack Limitations
    Managed to complete animations for the Run, Jump, Fall, Idle and Attack States, as well as a coded in 'isFalling' check that works both as a pre-requisite for the fall animation to play and my Dive Attack to be possible.
    
    I had planned on trying to get a sprite explosion to play behind the player as well as a particle effect to play around the player after completing their dive attack in this update, though that will need some more work as it bogged down my performance and did not play with the method I had used. The dive attack can be performed by holding down on the Down Arrow key while falling, to accelerate the player faster to the ground.
    
    However, I did code in some more limitations for the player during the attack animation. They are the followng:
    -Player cannot cast while in mid air
    -Player cannot move while casting
    -Player cannot jump while casting
    
    These should help in the future to keep things balanced.
    
    Next on the to do list will be to add projectiles to the player's cast, implement the shockwave to the dive attack, and to start some minor polish to move on to Enemies and such.
    
    -E.M

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float groundCheckRadius = 0.2f;
-    
-    [SerializeField] private bool isGrounded = false;
-    private LayerMask groundLayer;
 
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private bool isFalling = false;
+    [SerializeField] private bool isDiving = false;
+    [SerializeField] private bool isCasting = false;
+
+    private LayerMask groundLayer;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Collider2D col;
     private Animator anim;
 
     private Vector2 groundCheckPos => new Vector2(col.bounds.min.x + col.bounds.extents.x, col.bounds.min.y);
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -31,23 +32,43 @@ public class PlayerController : MonoBehaviour
             Debug.LogWarning("Ground Layer not set fool! Please set the Ground Layer in the LayerMask.");
         }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        float hValue = Input.GetAxis("Horizontal");
-        SpriteFlip(hValue);
-        
-        rb.linearVelocityX = hValue * 5f;
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        float hValue = Input.GetAxis("Horizontal");
+
+        if (!isCasting)
+        {
+            SpriteFlip(hValue);
+            rb.linearVelocityX = hValue * 5f;
+        }
+        else
+        {
+            rb.linearVelocityX = 0f;
+        }
+
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded && !isCasting)
         {
             rb.AddForce(Vector2.up * 9f, ForceMode2D.Impulse);
         }
+
         isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, groundLayer);
+        isFalling = rb.linearVelocity.y < 0 && !isGrounded;
+
+        bool downHeld = Input.GetKey(KeyCode.DownArrow);
+        isDiving = isFalling && downHeld;
+
+        if (Input.GetMouseButtonDown(0) && isGrounded && !isCasting)
+        {
+            anim.SetTrigger("Cast");
+            isCasting = true;
+        }
 
         anim.SetFloat("hValue", Mathf.Abs(hValue));
         anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isFalling", isFalling);
     }
 
     void SpriteFlip(float hValue)
@@ -55,4 +76,19 @@ public class PlayerController : MonoBehaviour
         if ((hValue > 0 && sr.flipX) || (hValue < 0 && !sr.flipX))
             sr.flipX = !sr.flipX;
     }
+
+    private void FixedUpdate()
+    {
+        if (isDiving)
+        {
+            rb.AddForce(Vector2.down * 35f);
+        }
+
+    }
+
+    public void EndCast()
+    {
+        isCasting = false;
+    }
+
 }
